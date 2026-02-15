8eb2aa3a50a5b67d559002d59032f6d8c5aac61f
Xeyyam456 <xeyyamelizade5@gmail.com>
2026-02-16 01:12:39 +0400

    movie end

--- assets/js/admin page script/movies-page.js
+++ assets/js/admin page script/movies-page.js
@@ -2,42 +2,26 @@
 
 
 
-
 //   delete modal
-document.getElementById('deleteModal').addEventListener('show.bs.modal', function (e) {
-    const btn = e.relatedTarget;
-    const title = btn.getAttribute('data-title');
-    document.getElementById('deleteItemName').textContent = '"' + title + '"';
-});
 
 
 
-// Edit modal dolu veziyyetde
+const deleteModalElement = document.getElementById('deleteModal')
+const deleteModal = new bootstrap.Modal(deleteModalElement)
 
-document.getElementById('contentModal').addEventListener('show.bs.modal', function (e) {
-    const btn = e.relatedTarget;
-    if (!btn.classList.contains('edit-btn')) return;
-
-    document.getElementById('editTitle').value = btn.dataset.title;
-    document.getElementById('editOverview').value = btn.dataset.overview;
-    document.getElementById('coverInput').value = btn.dataset.cover;
-    document.getElementById('posterImg').src = btn.dataset.cover;
-    document.getElementById('editImdb').value = btn.dataset.imdb;
-
-    const cat = btn.dataset.category;
-    const select = document.getElementById('catSelect');
-    select.value = cat;
-    select.classList.add('filled');
-});
+function showDeleteModal(id){
+    deleteModal.show()
+}
 
 
-// reset modal ve create 
+const editModalElement = document.getElementById("editModal");
+const editModal = new bootstrap.Modal(editModalElement)
+
+function showEditModal(id) {
+    currentEditId = id;     
+    editModal.show();
+}
+
+
  
-document.getElementById('contentModal').addEventListener('show.bs.modal', function (e) {
-    if (e.relatedTarget.classList.contains('create-btn')) {
-        document.querySelectorAll('#contentModal input, #contentModal textarea').forEach(i => i.value = '');
-        document.getElementById('posterImg').src = '';
-        document.getElementById('catSelect').selectedIndex = 0;
-    }
-});
 
