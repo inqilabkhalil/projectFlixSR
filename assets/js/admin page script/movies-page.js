bdee3d29a38873e0526e9eaaeec8cf1b709789dc
Xeyyam456 <xeyyamelizade5@gmail.com>
2026-02-18 23:46:56 +0400

    fix code

--- assets/js/admin page script/movies-page.js
+++ assets/js/admin page script/movies-page.js
@@ -9,7 +9,10 @@
 const deleteModalElement = document.getElementById('deleteModal')
 const deleteModal = new bootstrap.Modal(deleteModalElement)
 
+currentEditId = null;
+
 function showDeleteModal(id){
+    currentEditId = id;
     deleteModal.show()
 }
 
