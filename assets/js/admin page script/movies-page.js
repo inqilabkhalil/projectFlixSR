a397546879a57e12c189a92cdc73672594481cf9
aytacnurulu <aytacnurulu332@gmail.com>
2026-02-24 14:45:01 +0400

    Save changes

--- assets/js/admin page script/movies-page.js
+++ assets/js/admin page script/movies-page.js
@@ -1,20 +1,18 @@
 
 
+const createBtn = document.querySelector('.create-btn');
+const createMovieModal = new bootstrap.Modal(document.getElementById('createMovieModal'));
+const deleteMovieModal = new bootstrap.Modal(document.getElementById('deleteMovieModal'));
+const editBtn = document.querySelector('.edit-btn');
+const deleteBtn = document.querySelector('.delete-btn');
+createBtn.addEventListener('click', () => {
+    createMovieModal.show();
+});
 
-const deleteModalElement = document.getElementById('deleteModal')
-const deleteModal = new bootstrap.Modal(deleteModalElement)
+editBtn.addEventListener('click', () => {
+    createMovieModal.show();
+});
 
-currentEditId = null;
-
-function showDeleteModal(id){
-    currentEditId = id;
-    deleteModal.show()
-}
-
-const editModalElement = document.getElementById('editModal');
-const editModal = new bootstrap.Modal(editModalElement);
-
-function showEditModal(id) {
-  currentEditId = id;
-  editModal.show();
-}
+deleteBtn.addEventListener('click', () => {
+    deleteMovieModal.show();
+});
