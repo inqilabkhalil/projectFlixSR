f0634abbf8974ab1b5ab1dc568ca08e3c11bfb00
vaqif mohbaliyev <vagifmohbaliyev45@gmail.com>
2026-03-06 08:32:04 +0400

    home update

--- assets/js/client/login.js
+++ assets/js/client/login.js
@@ -1,27 +1,26 @@
-const login_url =
-  "https://api.sarkhanrahimli.dev/api/filmalisa/auth/login";
-const form = document.querySelector(".login-form");
-const warningEl = document.getElementById("warning");
+const login_url = 'https://api.sarkhanrahimli.dev/api/filmalisa/auth/login';
+const form = document.querySelector('.login-form');
+const warningEl = document.getElementById('warning');
 
 function showWarning(text) {
   warningEl.textContent = text;
-  warningEl.style.display = "block";
+  warningEl.style.display = 'block';
 }
 
 async function login(event) {
   event.preventDefault();
-  const email = document.getElementById("email").value.trim();
-  const password = document.getElementById("password").value;
+  const email = document.getElementById('email').value.trim();
+  const password = document.getElementById('password').value;
 
   if (!email || !password) {
-    showWarning("Email/username və şifrə boş ola bilməz");
+    showWarning('Email/username və şifrə boş ola bilməz');
     return;
   }
 
   const options = {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({ email, password }),
   };
@@ -30,15 +29,15 @@ async function login(event) {
     const response = await fetch(login_url, options);
     const responseData = await response.json();
     if (!responseData.result) {
-      showWarning("server xətası");
+      showWarning('server xətası');
       return;
     }
     const accessToken = responseData.data.tokens.access_token;
-    localStorage.setItem("client_token", accessToken);
-    window.location.href = "../client/home.html";
+    localStorage.setItem('client_token', accessToken);
+    window.location.href = '../client/home.html';
   } catch (error) {
-    showWarning("Server xətası");
+    showWarning('Server xətası');
   }
 }
 
-form.addEventListener("submit", login);
+form.addEventListener('submit', login);
